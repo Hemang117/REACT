8b041375a0c8abf29c87ed65c080f3befb72f49c
Hemang117 <hemangsaini46600@gmail.com>
2023-09-17 18:25:55 +0530

    git commit message

---
+++ src/MyComponents/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            Hi my name is Hemang Saini 
+            <p>And this is my first try at making a react website with the sole purpose to learn as much as i can</p>
+            <p> very generic website but hey its a try</p>
+        </div>
+    )
+}
---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+export const Footer = () => { 
+    return (
+        <footer className="bg-dark text-light py-4">
+            <p className="text-center">
+           This is a try at making a footer to work 
+           
+            </p>
+        </footer>
+    )
+}
